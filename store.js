e08e99bec53e5bf12c0e40ed1397629b1b482ff3
David Lenh <dlenh425@gmail.com>
2023-01-18 11:06:48 -0800

    Added ability to change quantity and have change reflected in cart total

--- store.js
+++ store.js
@@ -10,12 +10,26 @@ function ready() {
         const button = removeCartItemButtons[i];
         button.addEventListener("click", removeCartItem)
     }
+
+    const quantityInputs = document.getElementsByClassName("cart-quantity-input");
+    for (let i = 0; i < quantityInputs.length; i++) {
+        const input = quantityInputs[i];
+        input.addEventListener("change", quantityChange)
+    }
 }
 
 function removeCartItem(event) {
     const buttonClicked = event.target // whatever button we clicked on
             buttonClicked.parentElement.parentElement.remove();
             updateCartTotal();
+}
+
+function quantityChange(event) {
+    const input = event.target;
+    if (isNaN(input.value) || input.value <= 0) {
+        input.value = 1;
+    }
+    updateCartTotal();
 }
 
 function updateCartTotal() {
@@ -29,7 +43,8 @@ function updateCartTotal() {
         
         const price = parseFloat(priceElement.innerText.replace("$", ""));
         const quantity = parseFloat(quantityElement.value);
-        total = total + (price * quantity)
+        total = total + (price * quantity);
     }
+    total = Math.round(total * 100)/100;
     document.getElementsByClassName("cart-total-price")[0].innerText = "$" + total
 }
