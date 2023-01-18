b534786a6a462676f412827244eaff13e157ac4b
David Lenh <dlenh425@gmail.com>
2023-01-18 10:31:05 -0800

    Added script file with ability to remove buttons and update cart total

---
+++ store.js
@@ -0,0 +1,28 @@
+
+
+const removeCartItemButtons = document.getElementsByClassName("btn-danger");
+
+for (let i = 0; i < removeCartItemButtons.length; i++) {
+    const button = removeCartItemButtons[i];
+    button.addEventListener("click", function(event) {
+        const buttonClicked = event.target // whatever button we clicked on
+        buttonClicked.parentElement.parentElement.remove();
+        updateCartTotal();
+    })
+}
+
+function updateCartTotal() {
+    const cartItemContainer = document.getElementsByClassName("cart-items")[0];
+    const cartRows = cartItemContainer.getElementsByClassName("cart-row");
+    let total = 0;
+    for (let i = 0; i < cartRows.length; i++) {
+        const cartRow = cartRows[i];
+        const priceElement = cartRow.getElementsByClassName("cart-price")[0];
+        const quantityElement = cartRow.getElementsByClassName("cart-quantity-input")[0];
+        
+        const price = parseFloat(priceElement.innerText.replace("$", ""));
+        const quantity = parseFloat(quantityElement.value);
+        total = total + (price * quantity)
+    }
+    document.getElementsByClassName("cart-total-price")[0].innerText = "$" + total
+}
